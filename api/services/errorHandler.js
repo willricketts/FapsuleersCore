b0c5cc00c4b1a1192c52cb6f55f151846940d990
Will Ricketts <will@willricketts>
2014-11-11 12:13:00 -0500

    adding errorHandlers

---
+++ api/services/errorHandler.js
@@ -0,0 +1,13 @@
+module.exports = {
+    
+  serverError: serverError,
+  nullCollection: nullCollection    
+};
+
+function serverError(err, res) {
+    
+}
+
+function nullCollection(collection, res) {
+    
+}
