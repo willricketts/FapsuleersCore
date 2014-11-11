fd244198effa92559efe3d78387ed8acb26ee205
Will Ricketts <will@willricketts>
2014-11-11 14:08:52 -0500

    adding case handling for apiKeyController

--- api/controllers/ApiKeyController.js
+++ api/controllers/ApiKeyController.js
@@ -17,16 +17,25 @@ function addKey(req, res) {
 function doAddKey(req, res) {
     var b = req.body;
     User.findOne({ id: req.session.identity }, function(err, user) {
+        console.log(JSON.stringify(user));
         errorHandler.serverError(err, res);
-        ApiKey.create({ keyId: b.keyId, vCode: b.vCode, owner: user.id }, function(err, apikey) {
-            errorHandler.serverError(err, res);
-            console.log(JSON.stringify(apikey));
-            if(apiKey) {
-                req.flash('API key created!');
-                res.redirect('/dashboard');
+        ApiKey.findOne({ keyId: b.keyId, vCode: b.vCode }, function(err, key) {
+            if(!key) {
+                ApiKey.create({ keyId: b.keyId, vCode: b.vCode, owner: user.id }, function(err, apikey) {
+                    errorHandler.serverError(err, res);
+                    console.log(JSON.stringify(apikey));
+                    if(apiKey) {
+                        req.flash('API key created!');
+                        res.redirect('/dashboard');
+                    }
+                    else {
+                        res.serverError('API key could not be added. Go bitch at El Marrow.');
+                    }
+                });
             }
             else {
-                res.serverError('API key could not be added. Go bitch at El Marrow.');
+                req.flash('API key already exists in system. Go bitch at El Marrow');
+                res.redirect('/addkey');
             }
         });
     });
