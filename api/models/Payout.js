b1cf2c2c4bfd23ac35e3d7b1da674ebae7b62201
Will Ricketts <will@willricketts>
2014-11-22 21:07:19 -0500

    adding loot types to payout model

---
+++ api/models/Payout.js
@@ -0,0 +1,33 @@
+/**
+* Payout.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  adapter: 'mongo',
+  attributes: {
+    
+    ancientCoordinates: {
+      type: 'integer'
+    },
+    
+    meltedNanoribbons: {
+      type: 'integer'
+    },
+
+    neuralNetworkAnalyzer: {
+      type: 'integer'
+    },
+
+    sleeperDataLibrary: {
+      type: 'integer'
+    },
+
+    sleeperDroneAi: {
+      type: 'integer'
+    }
+  };
+
